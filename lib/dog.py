faccd4d342e8c373054b86be6f0c42178a1907f0
Virginia Gichira <gichiravirginia@gmail.com>
2024-05-29 02:10:04 +0300

    Implement attributes and propeties

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,26 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+# File: lib/dog.py
+
 class Dog:
-    pass
+    def __init__(self, name="unknown", breed="Mutt"):
+        self._name = None
+        self.name = name
+        self.breed = breed
+    def name(self):
+        # print (f"{self._name}")
+        return self._name
+    
+    def name(self, name):
+        if (type(name) in (str)) and 1 <= len(name) <= 25:
+            print(f"{name}")
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            return ""
+    def sit(self):
+        print(f"{self.name} is sitting.")
+    
+    def bark(self):
+        print(f"{self.name} says woof!")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,24 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+    def job(self):
+        return self._job
+    def job(self, value):
+        if value in self.approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
